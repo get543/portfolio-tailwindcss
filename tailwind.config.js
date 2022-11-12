67330548f64d8b31ce7f12375126e98a0f012077
Udin <fghaza.pratama@gmail.com>
2022-11-12 20:36:28 +0700

    add automatic dark mode switch (system prefered))

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         primary: "#06B6D4",
         secondary: "#64748B",
         dark: "#0F172A",
+        darkish: "rgb(18, 28, 53)"
       },
       screens: {
         "2xl": "1320px",
